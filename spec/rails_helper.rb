c3d04af09575337e66320289c0e29e117c2db02b
nevroje <joakim.berglind@gmail.com>
2019-04-25 13:20:47 +0200

    wrote first test, created db

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
-require 'capybara/rails'
+require 'capybara/rspec'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'capybara/rails'
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
