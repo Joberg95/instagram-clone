0f3e0459e3ecf0fa807273f53b66d0607b207b6c
nevroje <joakim.berglind@gmail.com>
2019-04-26 17:41:17 +0200

    added create method in controller and defined image params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,18 @@
 class PostsController < ApplicationController
+  def create
+  @post = Post.create(post_params)
+  end
+
   def index
   end
 
   def new
     @post = Post.new
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:caption, :image)
   end
 end
