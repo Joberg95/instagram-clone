617c84912de5124c12c9ae691e72c0ce54ed4c51
nevroje <joakim.berglind@gmail.com>
2019-04-26 17:48:17 +0200

    updated show and new in posts controller,  fixed create post tests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
 class PostsController < ApplicationController
+  
   def create
-  @post = Post.create(post_params)
+    @post = Post.create(post_params)
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
   def index
--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -6,7 +6,7 @@ feature 'Creating posts' do
     click_link 'New post'
     attach_file('Image', "spec/files/images/coffee.jpg")
     fill_in 'Caption', with: '#coffeetime'
-    click_button 'Create post'
+    click_button 'Create Post'
     expect(page).to have_content('#coffeetime')
     expect(page).to have_css("img[src*='coffee.jpg']")
   end
