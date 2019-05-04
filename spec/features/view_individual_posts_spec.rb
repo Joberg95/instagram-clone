45e274c2270a6e34cc6c81c9fe551be996faf760
nevroje <joakim.berglind@gmail.com>
2019-05-04 16:21:44 +0200

    added image as links in index, added post#id as link in tests to make it pass

--- spec/features/view_individual_posts_spec.rb
+++ spec/features/view_individual_posts_spec.rb
@@ -4,8 +4,8 @@ feature 'Can view individual posts' do
   scenario 'Can click to view single post' do
   post = create(:post)
   visit '/'
-  find(:xpath, "//a[contains(@href,'posts/1')]").click
+  find(:xpath, ".//a[@href='/posts/#{post.id}']").click
   expect(page.current_path).to eq(post_path(post))
-  
+
   end
 end
