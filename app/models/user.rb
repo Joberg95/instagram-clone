8769f7049aca38bad0170984f4ab0c251ee3ee6e
nevroje <joakim.berglind@gmail.com>
2019-05-15 12:58:34 +0200

    installed html2haml, and converted all devise views to haml

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :user_name, presence: true, length: { minimum: 4, maximum: 16 }
 end
